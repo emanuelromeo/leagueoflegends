d6b5dd7ce2cced56d0c73aa3709e7cca41ef1a7d
emanuelromeo <emanuel.romeo@hotmail.com>
2025-04-04 16:47:30 +0200

    Added crossOrigin

--- src/main/java/com/develhope/leagueoflegends/controller/AbilityController.java
+++ src/main/java/com/develhope/leagueoflegends/controller/AbilityController.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/abilities")
+@CrossOrigin
 public class AbilityController {
 
     @Autowired
--- src/main/java/com/develhope/leagueoflegends/controller/ChampionController.java
+++ src/main/java/com/develhope/leagueoflegends/controller/ChampionController.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/champions")
+@CrossOrigin
 public class ChampionController {
 
     @Autowired
